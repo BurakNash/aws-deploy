dbc0f9aa563e9e642bc7cdc3c57305c37e6b4ec8
Burak <burakkayakoc@gmail.com>
2020-06-15 16:35:13 -0500

    added connection string to startup

--- WebApi/WebApi/Data/DataContext.cs
+++ WebApi/WebApi/Data/DataContext.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi.Models;
 
 namespace WebApi.Data
 {
-    public class DataContext
+    public class DataContext: DbContext
     {
+        public DataContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+     
     }
 }
